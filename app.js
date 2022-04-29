c3b0d58513db656c844b3b3031e29fd425dc1b88
Leerena <leerena7@gmail.com>
2022-04-29 10:13:00 +0300

    added files

---
+++ app.js
@@ -0,0 +1,30 @@
+const board = document.querySelector('#board');
+const SQUARES_NUMBER = 700;
+const colors = ['#ee1bf9', '#f75b9c', '#9407bf', '#6837fc', '#379dfc', '#37fcf9', '#37fc99', '#41ff16', '#92ff16', '#ffff7a', '#ff2807', '#00e541'];
+
+for(let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => setColor(square));
+
+    square.addEventListener('mouseleave', () => removeColor(square));
+
+    board.append(square);
+}
+
+function setColor(element) {
+    const color = getRandomColor();
+    element.style.background = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+    element.style.background = '#1d1d1d';
+    element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+}
